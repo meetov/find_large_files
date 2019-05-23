754d609b22589f7093c1fc9a931d265be7cfdbef
meetov <mario.mitov@protonmail.com>
2019-05-23 17:56:14 +0300

    Make it process the result of find
    Until now was executing du cmd for every file which was very slow.

--- find_large_files.sh
+++ find_large_files.sh
@@ -8,13 +8,22 @@ START_DIR=$1
 LIMIT=$2
 DEFAULT_LIMIT=10
 
-if [ ! $START_DIR ]; then
-	echo "Usage: $SCRIPT_NAME START_DIR [LIMIT]"
+if [ ! ${START_DIR} ]; then
+	echo "Usage: ${SCRIPT_NAME} START_DIR [LIMIT]"
 	exit 1
 fi
 
 if [ ! $LIMIT ]; then
-	LIMIT=$DEFAULT_LIMIT
+	LIMIT=${DEFAULT_LIMIT}
 fi
 
-find $START_DIR -type f -exec du -hs {} \;  2>/dev/null | sort -h | tail -n $LIMIT
+RESULTS=$(find ${START_DIR} -type f -printf "%s;%p\n"  2>/dev/null | sort -h | tail -n ${LIMIT})
+
+for RESULT in ${RESULTS}
+do
+	FILE_SIZE_B=$(echo "${RESULT}" | cut -d ';' -f 1)
+	FILE_NAME=$(echo "${RESULT}" | cut -d ';' -f 2)
+	FILE_SIZE_MB=$(( ${FILE_SIZE_B} / 1024 / 1024 ))
+	echo "${FILE_SIZE_MB}M\t${FILE_NAME}"
+done
+
